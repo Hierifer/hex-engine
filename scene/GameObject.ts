82fbd5135eec9d62b9e1344f6bd258a16128a011
Hierifer <hierifer@hotmail.com>
2024-08-08 02:33:42 +0800

    chore: 收敛添加和删除逻辑

--- scene/GameObject.ts
+++ scene/GameObject.ts
@@ -1,37 +1,55 @@
-import { Sprite } from 'pixi.js';
+import { Component, Physics2DComponent } from "./Component";
+import { v4 as uuidv4 } from "uuid";
 
 export class GameObject {
-    id: number = 0
-    geo: GO_GEOMETRY = {posX: 0, posY:0, rotX: 0, rotY: 0, scaleX:0, scaleY: 0}
-    attrs = {width: 100, height: 100}
-    components: Array<Sprite> = []
-    constructor(id: number, geo: Partial<GO_GEOMETRY>, attrs: Partial<{width: number, height: number}>){
-        this.id = id
-        this.geo = {...this.geo, ...geo}
-        this.attrs = {...this.attrs, ...attrs}
+  id = "";
+  geo: GO_GEOMETRY = {
+    posX: 0,
+    posY: 0,
+    rotX: 0,
+    rotY: 0,
+    scaleX: 0,
+    scaleY: 0,
+  };
+  attrs = { width: 100, height: 100 };
+  components: Array<Component> = [];
+  constructor(
+    geo: Partial<GO_GEOMETRY>,
+    attrs: Partial<{ width: number; height: number }>
+  ) {
+    this.id = uuidv4();
+    this.geo = { ...this.geo, ...geo };
+    this.attrs = { ...this.attrs, ...attrs };
+  }
+  _mountd() {}
+  _destory() {}
+  start() {}
+  update() {}
+  addComponents(comps: Array<Component>) {
+    this.components = [...this.components, ...comps];
+    return this;
+  }
+  setPosition(x: number, y: number) {
+    this.geo.posX = x;
+    this.geo.posY = y;
+  }
+  getId() {
+    return this.id;
+  }
+  findComponent(label: string) {
+    return this.components.find((comp) => {
+      return comp.label === label;
+    });
+  }
+  getPhysics2DBody() {
+    console.log(this.components);
+    const pcomp = this.findComponent("physics2d");
+    if (pcomp !== void 0 && pcomp instanceof Physics2DComponent) {
+      return pcomp.getBody();
+    } else {
+      return null;
     }
-    addSprite(sprite: Sprite){
-        this.components.push(sprite)
-        return this
-    }
-    _mountd(){
-
-    }
-    _destory(){
-
-    }
-    start(){
-
-    }
-    update(){
-
-    }
-    addComponents(){
-
-    }
-    removeComponents(){
-
-    }
+  }
+  removeComponents() {}
 }
-export default GameObject
-
+export default GameObject;
